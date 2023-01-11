44603f052f4e10a3efcbc96cec530657a6d3f5cb
lvp0110 <110590002+lvp0110@users.noreply.github.com>
2023-01-11 12:08:20 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,23 @@
+document.getElementById("quest12").onclick = function () {
+    var chiller = document.getElementById("chiller");
+    var chiller_1 = document.getElementById("chiller_1");
+    var quest12 = document.getElementById("quest12");
+    var blockVRV = document.getElementById("blockVRV");
+    
+
+    if(chiller.style.display === "none"){
+        chiller_1.style.display="inline";
+        blockVRV.style.display="inline";
+        quest12.innerHTML=" ";
+        chiller.style.display="none";
+
+    }  else  {
+        chiller_1.style.display="none";
+        quest12.innerHTML="X";
+        chiller.style.display="inline";
+        blockVRV.style.display="inline";
+
+    } 
+    
+}
+ 
